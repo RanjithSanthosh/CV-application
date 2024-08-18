1f187988dd87b781d6b2f541eb15f1b6d8ad710e
RanjithSanthosh <ranjithsanthosh66@gmail.com>
2024-08-18 15:39:57 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Section3 from "./components/Section3";
 
 function App() {
   return (
+    
     <div className="bg-orange-400 font-semibold h-lvh">
       <h1 className="text-black text-4xl font-bold p-4 flex justify-center">CV Form</h1>
       {/* <Section1/> */}
